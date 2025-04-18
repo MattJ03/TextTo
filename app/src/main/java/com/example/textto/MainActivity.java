d81447dfd2a13489278216fbc754f8719635c0b4
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-18 11:54:25 +0100

    start of main

--- app/src/main/java/com/example/textto/MainActivity.java
+++ app/src/main/java/com/example/textto/MainActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username, password, time;
+        Button logInBtn;
+
+        password = findViewById(R.id.editTextTextPassword);
+        logInBtn = findViewById(R.id.button);
+
 
     }
 }
