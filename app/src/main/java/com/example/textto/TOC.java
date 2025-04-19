a4383b90e88da8992992439c6c9a0765322b9122
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-19 20:07:35 +0100

    back button fun for toc

--- app/src/main/java/com/example/textto/TOC.java
+++ app/src/main/java/com/example/textto/TOC.java
@@ -2,11 +2,27 @@ package com.example.textto;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class TOC extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.toc);
+
+        Button backBtn;
+
+        backBtn = findViewById(R.id.button3);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TOC.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
