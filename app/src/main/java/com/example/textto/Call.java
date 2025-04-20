bd3f6fcf91f9571d5445a018ba55486b6029262c
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-20 13:26:28 +0100

    back button func

--- app/src/main/java/com/example/textto/Call.java
+++ app/src/main/java/com/example/textto/Call.java
@@ -1,17 +1,26 @@
 package com.example.textto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.os.Bundle;
+import android.Manifest;
+
+import java.security.Permission;
 
 public class Call extends AppCompatActivity {
 
     EditText name, number;
     Button backBtn, callBtn;
+    private static final int CALL_PHONE_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +37,33 @@ public class Call extends AppCompatActivity {
         callBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(name.getText().toString().isEmpty() && !number.getText().toString().isEmpty()) {
-                    Toast.makeText(Call.this, "Enter a name to send to", Toast.LENGTH_LONG).show();
-                } else if(!name.getText().toString().isEmpty() && number.getText().toString().isEmpty()) {
-                    Toast.makeText(Call.this, "Enter a number to call", Toast.LENGTH_LONG).show();
-                } else if(name.getText().toString().isEmpty() && number.getText().toString().isEmpty()) {
-                    Toast.makeText(Call.this, "Enter a name and number", Toast.LENGTH_LONG).show();
+                if (ActivityCompat.checkSelfPermission(Call.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+                    if (name.getText().toString().isEmpty() && !number.getText().toString().isEmpty()) {
+                        Toast.makeText(Call.this, "Enter a name to send to", Toast.LENGTH_LONG).show();
+                    } else if (!name.getText().toString().isEmpty() && number.getText().toString().isEmpty()) {
+                        Toast.makeText(Call.this, "Enter a number to call", Toast.LENGTH_LONG).show();
+                    } else if (name.getText().toString().isEmpty() && number.getText().toString().isEmpty()) {
+                        Toast.makeText(Call.this, "Enter a name and number", Toast.LENGTH_LONG).show();
+                    } else {
+                        Intent intent = new Intent(Intent.ACTION_CALL);
+                        intent.setData(Uri.parse("tel:" + number.getText().toString()));
+                        startActivity(intent);
+                    }
                 } else {
-                    Intent intent = new Intent(Intent.ACTION_C)
+                    ActivityCompat.requestPermissions(Call.this, new String[]{Manifest.permission.CALL_PHONE}, CALL_PHONE_PERMISSIONS);
                 }
             }
         });
 
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number.setText("");
+                name.setText("");
+                Intent intent = new Intent(Call.this, Choose.class);
+                startActivity(intent);
+            }
+        });
     }
 }
