66771fa994e1bacbebec6a051d984a2a6d8e4508
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-20 12:39:22 +0100

    manifest permissions for calling and sms

--- app/src/main/java/com/example/textto/Call.java
+++ app/src/main/java/com/example/textto/Call.java
@@ -10,9 +10,35 @@ import android.os.Bundle;
 
 public class Call extends AppCompatActivity {
 
+    EditText name, number;
+    Button backBtn, callBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.call);
+
+        name = findViewById(R.id.editTextCallName);
+        number = findViewById(R.id.editTextPhone2);
+        backBtn = findViewById(R.id.buttonBackCall);
+        callBtn = findViewById(R.id.buttonCall);
+
+        name.hasFocus();
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().isEmpty() && !number.getText().toString().isEmpty()) {
+                    Toast.makeText(Call.this, "Enter a name to send to", Toast.LENGTH_LONG).show();
+                } else if(!name.getText().toString().isEmpty() && number.getText().toString().isEmpty()) {
+                    Toast.makeText(Call.this, "Enter a number to call", Toast.LENGTH_LONG).show();
+                } else if(name.getText().toString().isEmpty() && number.getText().toString().isEmpty()) {
+                    Toast.makeText(Call.this, "Enter a name and number", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_C)
+                }
+            }
+        });
 
     }
 }
